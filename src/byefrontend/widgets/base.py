fb63f50e9bed3cece2daa4801147abbe14686c47
nommu-moose <moose@nommumail.co.uk>
2024-12-10 14:54:33 +0100

    generalising overall widget class for simplicity

--- src/byefrontend/widgets/base.py
+++ src/byefrontend/widgets/base.py
@@ -9,10 +9,136 @@ from django.utils.html import format_html, format_html_join
 from django.utils.safestring import mark_safe
 
 
+class BFEChildrenDict(dict):
+    # in future make it force all instances to only have one dictionary they're part of?
+    # # # this would need a parent_dict attr too.
+    # auto set parents of sub objects?
+    def __init__(self, parent, *args, **kwargs):
+        self.parent = parent
+        self.mark_parent_for_recache()
+        super().__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        # If adding or replacing a child widget, ensure it knows about its parent
+        if hasattr(value, 'parent'):
+            value.parent = self.parent
+        super().__setitem__(key, value)
+        # Since we've changed the structure of children, mark parent as needing recache
+        self.mark_parent_for_recache()
+
+    def __delitem__(self, key):
+        super().__delitem__(key)
+        # Removing a child also affects rendering
+        self.mark_parent_for_recache()
+
+    def clear(self):
+        super().clear()
+        self.mark_parent_for_recache()
+
+    def pop(self, key, default=None):
+        val = super().pop(key, default)
+        self.mark_parent_for_recache()
+        return val
+
+    def popitem(self):
+        val = super().popitem()
+        self.mark_parent_for_recache()
+        return val
+
+    def update(self, *args, **kwargs):
+        super().update(*args, **kwargs)
+        # On updating multiple items, also ensure recache
+        self.mark_parent_for_recache()
+
+    def mark_parent_for_recache(self):
+        self.parent.needs_render_recache = True
+        self.parent._needs_media_recache = True
+
+
 class BFEBaseWidget:
+    # if render is passed attrs, don't use cache at all - assume unique
+    DEFAULT_CACHE_RELEVANT_ATTRS = {
+        'name', 'id', 'classes', 'attrs', 'value', 'label', 'help_text',
+        'required', 'widget_type', 'children', 'aria_label'
+    }
+    cache_relevant_attrs = DEFAULT_CACHE_RELEVANT_ATTRS
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', 'widget')
+        self.id = kwargs.get('id', self.generate_id())
+        self.classes = kwargs.get('classes', [])
+        self.attrs = kwargs.get('attrs', {})
+        self.value = kwargs.get('value')
+        self.label = kwargs.get('label', None)
+        self.help_text = kwargs.get('help_text', None)
+        self.required = kwargs.get('required', False)
+        self.widget_type = kwargs.get('widget_type', 'text')
+        self.parent = kwargs.pop('parent', None)
+        self._needs_render_recache = True
+        self.cached_render = ''
+        self.cached_media = None
+        self._needs_media_recache = True
+        self._children = BFEChildrenDict(self)
+
+    def generate_id(self):
+        return f"{self.name}_{str(uuid.uuid4())}"
+
+    @property
+    def children(self):
+        return self._children
+
+    @children.setter
+    def children(self, value: dict):
+        if not isinstance(value, dict):
+            raise TypeError('children must be a dict')
+        self._children.clear()
+        self._children.update(value)
+
+    def __setattr__(self, name, value):
+        if name in self.cache_relevant_attrs:
+            self._needs_render_recache = True
+        super().__setattr__(name, value)
+
+    @property
+    def needs_render_recache(self):
+        return self._needs_render_recache
+
+    @needs_render_recache.setter
+    def needs_render_recache(self, value: bool):
+        if value:
+            self._needs_render_recache = True
+            if self.parent is not None:
+                self.parent.needs_render_recache = True
+
+    def render(self, *args, **kwargs):
+        if self.needs_render_recache:
+            self.cached_render = self._render(args, kwargs)
+        return self.cached_render
+
+    def _render(self, *args, **kwargs):
+        return self.cached_render
+
     @property
     def media(self):
-        return Media(css=self.Media.css, js=self.Media.js)
+        """
+        Returns a Media object that aggregates both this widget's media and its children's media.
+        If the media needs recaching, it will be recomputed.
+        """
+        if self._needs_media_recache:
+            self.cached_media = self._compute_media()
+            self._needs_media_recache = False
+        return self.cached_media
+
+    def _compute_media(self):
+        """
+        Compute the merged Media for this widget and all of its children.
+        """
+        own_media = Media(css=self.Media.css, js=self.Media.js)
+        child_medias = [child.media for child in self.children.values() if hasattr(child, 'media')]
+        # Merge all children's media into own_media
+        for cm in child_medias:
+            own_media += cm
+        return own_media
 
     class Media:
         css = {}
@@ -95,7 +221,7 @@ class SecretToggleCharWidget(PasswordInput):
         js = ('byefrontend/js/secret_field.js',)
 
 
-class HyperlinkWidget:
+class HyperlinkWidget(BFEBaseWidget):
     def __init__(self, link: str, text: str, classes: list = None, reverse_args: list[str] = None,
                  edit_visible: bool = True, view_visible: bool = True, parent=None):
         self.parent = parent
@@ -112,10 +238,59 @@ class HyperlinkWidget:
         self.edit_visible = edit_visible
         self.view_visible = view_visible
 
-    def get_html(self, view_mode):
-        if not [self.edit_visible, self.view_visible][view_mode]:
-            return None
+    def __str__(self):
+        return self.render()
+
+    def render(self, attrs=None, renderer=None, *args, **kwargs):  # old: (self, name, value, attrs=None, renderer=None)
+        """
+        Renders the widget as HTML with a toggle button.
+
+        :param name: The name of the input.
+        :param value: The value of the input.
+        :param attrs: Additional HTML attributes.
+        :param renderer: The renderer to use.
+        :return: Safe HTML string.
+        """
+        if attrs is None:
+            attrs = {}
+        # Generate a unique ID for the widget instance
+        unique_id = uuid.uuid4().hex
+        html_id = attrs.get('id', f'id_{name}_{unique_id}')
+        attrs['id'] = html_id
+
         classes = ' '.join(['btn'] + self.classes)
         text = self.text
         link = reverse(self.link, args=self.reverse_args)
         return mark_safe(f'<a href="{link}" class="{classes}">{text}</a>')
+
+
+class TinyThumbnailWidget:
+    pass
+
+
+class TitleWidget:
+    pass
+
+
+class InputFieldWidget:
+    pass
+
+
+class BoxInputWidget:
+    pass
+
+
+class TextInputWidget:
+    pass
+
+
+class LabelWidget:
+    pass
+
+
+class CheckBoxWidget:
+    pass
+
+
+class RadioWidget:
+    pass
--- src/byefrontend/widgets/files.py
+++ src/byefrontend/widgets/files.py
@@ -1,64 +1,131 @@
 from django.utils.safestring import mark_safe
-
 from byefrontend.widgets import BFEBaseWidget
+import json
 
 
 class FileUploadWidget(BFEBaseWidget):
-    # to be changed to have option of a utility class so that no instantiation is needed - optimisation for threads
     aria_label = "File upload widget"
     inline_text = "Drop files here or click to upload."
 
     def __init__(self, config=None, parent=None, attrs=None, *args, **kwargs):
         """
-        Initializes the FileUploadWidget.
+        Initializes the FileUploadWidget with extended config.
 
-        :param attrs: HTML attributes to customize the navbar (e.g., class, id).
-        :param args: Additional positional arguments (not currently used).
-        :param kwargs: Additional keyword arguments (reserved for future use or subclassing).
+        Expected config keys:
+        - upload_url: str, URL to submit files to
+        - widget_html_id: str, unique HTML ID for this widget
+        - filetypes_accepted: list of MIME strings, e.g. ['image/png','image/jpeg']
+        - auto_upload: bool, if True files upload immediately
+        - can_upload_multiple_files: bool, if True multiple files can be added
+        - fields: list of dict for metadata fields if auto_upload=False
+                  Each field: {"field_name": "title", "editable": true, "visible": true}
+                  'file_name' is a default field always included.
+
+        :param config: configuration dictionary
+        :param parent: parent widget (if any)
+        :param attrs: HTML attributes
         """
         if config is None:
             config = {}
-        self.name = config.get('name', 'Untitled Navbar')
-        self.text = config.get('text', 'Untitled Site')
-        self.title_button = config.get('title_button', False)
-        self.link = config.get('link', None)
+        self.config = {
+            'upload_url': config.get('upload_url', ''),
+            'widget_html_id': config.get('widget_html_id', 'file_upload_widget'),
+            'filetypes_accepted': config.get('filetypes_accepted', []),
+            'auto_upload': config.get('auto_upload', True),
+            'can_upload_multiple_files': config.get('can_upload_multiple_files', True),
+            'fields': config.get('fields', [])
+        }
+
+        self.name = config.get('name', 'Untitled Upload Widget')
 
         self.parent_navbar = parent
 
         attrs = attrs or {}
-
         existing_classes = attrs.get('class', '')
         updated_classes = f"{existing_classes} bfe-navbar".strip()
-
         attrs['class'] = updated_classes
-
         self.attrs = attrs
 
     def __str__(self):
         return self.render()
 
-    def create_data_json(self, selected_path=None, first_recur=True):
-        file_upload_data = {}
-
-        return file_upload_data
+    def create_data_json(self):
+        # Create a data JSON structure for the frontend.
+        return {
+            'upload_url': self.config['upload_url'],
+            'widget_html_id': self.config['widget_html_id'],
+            'filetypes_accepted': self.config['filetypes_accepted'],
+            'auto_upload': self.config['auto_upload'],
+            'can_upload_multiple_files': self.config['can_upload_multiple_files'],
+            'fields': self.config['fields']
+        }
 
     def render(self, attrs=None, renderer=None):
         if attrs is None:
             attrs = {}
 
-        data_list = self.create_data_json()
+        data_config = self.create_data_json()
+        data_json = json.dumps(data_config)
 
-        # data_json_escaped = json.dumps(data_list, indent=4)
-        # need to add data json
-        file_upload_html = \
-            f'''
-            <div class="file-upload-wrapper">
-                <div id="drop-zone">{self.inline_text}</div>
-                <input type="file" id="file-input" multiple style="display:none;">
-                <div id="upload-list"></div>
-                <div id="messages"></div>
+        # Construct columns for fields: always have file_name, plus any additional fields
+        fields = self.config['fields'] if not self.config['auto_upload'] else []
+        # We'll always have a file_name column. Make sure it's visible:
+        # Insert file_name at start if not present.
+        has_file_name = any(f['field_name'] == 'file_name' for f in fields)
+        if not has_file_name:
+            fields.insert(0, {'field_name': 'file_name', 'editable': False, 'visible': True})
+        # We'll have a thumbnail column as well. Prepend that:
+        # Insert thumbnail column at start for display
+        fields.insert(0, {'field_name': 'thumbnail', 'editable': False, 'visible': True})
+
+        fields_html_headers = "".join(
+            f"<th>{f['field_name']}</th>" for f in fields if f.get('visible', True)
+        )
+
+        # For "To Upload" and "Uploaded" lists, we show a table. If auto_upload=False,
+        # "To Upload" table is editable. "Uploaded" table is read-only.
+        upload_button_html = ""
+        if not self.config['auto_upload']:
+            upload_button_html = '<button type="button" id="upload-all-btn">Upload All</button>'
+
+        file_upload_html = f'''
+        <div id="{self.config['widget_html_id']}" class="file-upload-wrapper" 
+             data-config='{data_json}'>
+
+            <div id="drop-zone">{self.inline_text}</div>
+            <input type="file" id="file-input" {'multiple' if self.config['can_upload_multiple_files'] else ''} style="display:none;"
+            {'accept="' + ','.join(self.config['filetypes_accepted']) + '"' if self.config['filetypes_accepted'] else ''}>
+
+            {upload_button_html}
+
+            <div id="lists-container">
+                <h3>To Upload</h3>
+                <table id="to-upload-list" class="upload-table">
+                    <thead>
+                        <tr>
+                            {fields_html_headers}
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+
+                <h3>Uploaded</h3>
+                <table id="uploaded-list" class="upload-table">
+                    <thead>
+                        <tr>
+                            {fields_html_headers}
+                            <th>Filepath</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
             </div>
-            '''
+
+            <div id="messages"></div>
+        </div>
+        '''
 
         return mark_safe(file_upload_html)
 
